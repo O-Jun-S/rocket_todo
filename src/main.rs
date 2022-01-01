5180c3557e68004df0d227da265be4cb1912e5b7
O-Jun-S <Jun.Shima256@gmail.com>
2022-01-01 04:43:42 +0000

    Print the description of the tasks..

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate rocket_dyn_templates;
 extern crate serde;
 
 use rocket::form::Form;
+use rocket::http::{CookieJar, Cookie};
 use rocket_dyn_templates::Template;
 
 
@@ -20,10 +21,11 @@ fn index() -> Template {
 }
 
 
+// number: the numbe of todo tasks
 #[rocket::post("/create", data = "<todo>")]
 async fn create_todo(todo: Option<Form<models::Todo<'_>>>) -> String {
     match todo {
-        Some(_todo) => "A todo has just been created!".to_string(),
+        Some(todo) => format!("A todo: {} has just been created!", todo.description),
         None => "Failed to make todo.".to_string(),
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -3,7 +3,7 @@ use rocket::form::{FromForm};
 #[derive(FromForm)]
 pub struct Todo<'r> {
     #[field(validate = len(1..))]
-    description: &'r str,
+    pub description: &'r str,
 
     #[field(name = "done")]
     completed: bool,
