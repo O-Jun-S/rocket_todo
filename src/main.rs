2affa43e5b9aa8d1b8250da2d70bf6d8763be0e7
O-Jun-S <Jun.Shima256@gmail.com>
2022-01-01 02:23:21 +0000

    Hello, World!

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use] extern crate rocket;
+
+#[rocket::get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+
+#[rocket::main]
+async fn main() {
+    let app = rocket::build()
+            .mount("/", rocket::routes![index])
+            .launch()
+            .await;
+    
+    match app {
+        Ok(()) => println!("Rocket launched successfully."),
+        _ => println!("Some errors occured...")
+    }
+}
