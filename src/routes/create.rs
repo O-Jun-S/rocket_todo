9e220bf7e5f4493f5161640a3d1fbd1ca5993c8a
O-Jun-S <Jun.Shima256@gmail.com>
2022-01-01 14:27:46 +0000

    Can create one task and store into cookie.

--- src/routes/create.rs
+++ src/routes/create.rs
@@ -1,17 +1,22 @@
 use crate::routes::models;
 
 use rocket::form::Form;
+use rocket::http::{Cookie, CookieJar};
 
 
 // number: the number of todo tasks
 #[rocket::post("/create", data = "<todo_desc>")]
-pub async fn create_todo(todo_desc: Option<Form<models::TodoDesc<'_>>>) -> rocket::serde::json::Json<models::Todo> {
+pub async fn create_todo(
+    jar: &CookieJar<'_>,
+    todo_desc: Option<Form<models::TodoDesc<'_>>>,
+) -> String {
     let desc: String = match todo_desc {
         Some(d) => d.into_inner().description.to_string(),
         None => "".to_string(),
     };
 
     let todo = models::Todo{ description: desc, completed: false };
+    jar.add(Cookie::new("body", serde_json::to_string(&todo).unwrap()));
 
-    rocket::serde::json::Json(todo)
+    serde_json::to_string(&todo).unwrap()
 }
